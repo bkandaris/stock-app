aa0d9eb6ecf5d2eea79aa966aaf108ebc0d2d563
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-28 15:42:02 -0500

    functional

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Stock from './components/Stock';
 function App() {
   return (
     <div className='App'>
+      <h1>Stock Market Chart</h1>
       <Stock />
     </div>
   );
--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Plotly from 'plotly.js';
+import createPlotlyComponent from 'react-plotly.js/factory';
 
 const Stock = () => {
   const [apiData, setApiData] = useState();
   const [stockChartXValues, setStockChartXValues] = useState([]);
   const [stockChartYValues, setStockChartYValues] = useState([]);
-  const [stock, setStock] = useState('F');
+  const [stockTicker, setStockTicker] = useState();
+  const [stock, setStock] = useState('FB');
+  const Plot = createPlotlyComponent(Plotly);
 
   const API_KEY = '4J02TAATISR9F7W6';
   let API = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stock}&outputsize=compact&apikey=${API_KEY}`;
@@ -15,30 +19,59 @@ const Stock = () => {
       .get(API)
       .then((res) => {
         setApiData(res.data);
-        console.log('apiData state', apiData);
+        console.log('res', res.data);
         let timeSeries = Object.values(apiData['Time Series (Daily)']);
-        console.log('timeSeries', timeSeries);
+
         let openDates = timeSeries.map(() => {
           return Object.keys(apiData['Time Series (Daily)']);
         });
-        console.log('openDates', openDates[0]);
+
         setStockChartXValues(openDates[0]);
-        console.log('stockChartXValues', stockChartXValues);
 
         let openPrices = timeSeries.map((e) => {
           return e['1. open'];
         });
+
         setStockChartYValues(openPrices);
-        console.log('stockChartYValues', stockChartYValues);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [stock, API]);
 
+  const handleChange = (e) => {
+    e.preventDefault();
+    setStockTicker(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setStock(stockTicker);
+  };
+  console.log('stock', stock);
   return (
     <div>
-      <h1>Stock</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder='enter ticker symbol here'
+          type='text'
+          name='stock'
+          onChange={handleChange}
+        />
+        <button type='submit'>Change Ticker</button>
+      </form>
+      <Plot
+        data={[
+          {
+            x: stockChartXValues,
+            y: stockChartYValues,
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: { color: 'red' },
+          },
+        ]}
+        layout={{ width: 720, height: 440, title: { stock } }}
+      />
     </div>
   );
 };
